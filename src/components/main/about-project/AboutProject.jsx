75e088f900c6720cd84968a00c32e125fc4eaf20
Max Limanskii <maxlivi@yandex.ru>
2023-08-16 11:50:31 +0300

    feat: добавить секцию AboutProject

--- src/components/main/about-project/AboutProject.jsx
+++ src/components/main/about-project/AboutProject.jsx
@@ -10,7 +10,7 @@ export default function AboutProject() {
           <h3 className="about-project__stage-title">
             Дипломный проект включал 5 этапов
           </h3>
-          <p className="about-project__description">
+          <p className="about-project__stage-description">
             Составление плана, работу над бэкендом, вёрстку, добавление
             функциональности и финальные доработки.
           </p>
@@ -20,12 +20,18 @@ export default function AboutProject() {
             <h3 className="about-project__stage-title">
               На выполнение диплома ушло 5 недель
             </h3>
-            <p className="about-project__description">
+            <p className="about-project__stage-description">
               У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
               соблюдать, чтобы успешно защититься.
             </p>
           </div>
         </div>
+      </div>
+      <div className="about-project__stages-grid">
+        <div className="about-project__period about-project__period_style_green">1 неделя</div>
+        <div className="about-project__period about-project__period_style_black">4 недели</div>
+        <span className="about-project__sphere">Back-end</span>
+        <span className="about-project__sphere">Front-end</span>
       </div>
     </section>
   );
